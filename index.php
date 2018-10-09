f546884edbd73c3b56fb7e826124ca58020d4c7c
chrisandyarief <chrisandyarief33@gmail.com>
2018-10-09 15:18:43 +0700

    add example html file

--- index.php
+++ index.php
@@ -10,71 +10,77 @@ if ((float)PCRE_VERSION<7.9)
 // Load configuration
 $f3->config('config.ini');
 
-$f3->route('GET /',
-	function($f3) {
-		$classes=array(
-			'Base'=>
-				array(
-					'hash',
-					'json',
-					'session',
-					'mbstring'
-				),
-			'Cache'=>
-				array(
-					'apc',
-					'apcu',
-					'memcache',
-					'memcached',
-					'redis',
-					'wincache',
-					'xcache'
-				),
-			'DB\SQL'=>
-				array(
-					'pdo',
-					'pdo_dblib',
-					'pdo_mssql',
-					'pdo_mysql',
-					'pdo_odbc',
-					'pdo_pgsql',
-					'pdo_sqlite',
-					'pdo_sqlsrv'
-				),
-			'DB\Jig'=>
-				array('json'),
-			'DB\Mongo'=>
-				array(
-					'json',
-					'mongo'
-				),
-			'Auth'=>
-				array('ldap','pdo'),
-			'Bcrypt'=>
-				array(
-					'mcrypt',
-					'openssl'
-				),
-			'Image'=>
-				array('gd'),
-			'Lexicon'=>
-				array('iconv'),
-			'SMTP'=>
-				array('openssl'),
-			'Web'=>
-				array('curl','openssl','simplexml'),
-			'Web\Geo'=>
-				array('geoip','json'),
-			'Web\OpenID'=>
-				array('json','simplexml'),
-			'Web\Pingback'=>
-				array('dom','xmlrpc')
-		);
-		$f3->set('classes',$classes);
-		$f3->set('content','welcome.htm');
-		echo View::instance()->render('layout.htm');
-	}
-);
+// $f3->route('GET /',
+// 	function($f3) {
+// 		$classes=array(
+// 			'Base'=>
+// 				array(
+// 					'hash',
+// 					'json',
+// 					'session',
+// 					'mbstring'
+// 				),
+// 			'Cache'=>
+// 				array(
+// 					'apc',
+// 					'apcu',
+// 					'memcache',
+// 					'memcached',
+// 					'redis',
+// 					'wincache',
+// 					'xcache'
+// 				),
+// 			'DB\SQL'=>
+// 				array(
+// 					'pdo',
+// 					'pdo_dblib',
+// 					'pdo_mssql',
+// 					'pdo_mysql',
+// 					'pdo_odbc',
+// 					'pdo_pgsql',
+// 					'pdo_sqlite',
+// 					'pdo_sqlsrv'
+// 				),
+// 			'DB\Jig'=>
+// 				array('json'),
+// 			'DB\Mongo'=>
+// 				array(
+// 					'json',
+// 					'mongo'
+// 				),
+// 			'Auth'=>
+// 				array('ldap','pdo'),
+// 			'Bcrypt'=>
+// 				array(
+// 					'mcrypt',
+// 					'openssl'
+// 				),
+// 			'Image'=>
+// 				array('gd'),
+// 			'Lexicon'=>
+// 				array('iconv'),
+// 			'SMTP'=>
+// 				array('openssl'),
+// 			'Web'=>
+// 				array('curl','openssl','simplexml'),
+// 			'Web\Geo'=>
+// 				array('geoip','json'),
+// 			'Web\OpenID'=>
+// 				array('json','simplexml'),
+// 			'Web\Pingback'=>
+// 				array('dom','xmlrpc')
+// 		);
+// 		$f3->set('classes',$classes);
+// 		$f3->set('content','welcome.htm');
+// 		echo View::instance()->render('layout.htm');
+// 	}
+// );
+
+$f3->route('GET /',function($f3){
+	$f3->set('content','home.html');
+	$view=new View;
+    echo $view->render('home.html');
+});
 
 $f3->route('GET /userref',
 	function($f3) {
@@ -82,5 +88,11 @@ $f3->route('GET /userref',
 		echo View::instance()->render('layout.htm');
 	}
 );
+
+$f3->set("DB",new DB\SQL(
+    'mysql:host=localhost;port=3306;dbname=id7413042_bukantokosebelah',
+    'id7413042_bukantokosebelah',
+    'admin'
+));
 
 $f3->run();
