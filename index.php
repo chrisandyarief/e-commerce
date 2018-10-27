cda6d87113c2840a215f44c456a912e68ec25bbc
chrisandyarief <chrisandyarief33@gmail.com>
2018-10-27 12:28:57 +0700

    add to cart

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 // Kickstart the framework
 $f3=require('lib/base.php');
 
@@ -10,65 +10,109 @@ if ((float)PCRE_VERSION<7.9)
 // Load configuration
 $f3->config('config.ini');
 
+//user interface routing
 $f3->route('GET /',function($f3){
 	$result=$f3->get("DB")->exec(
-        'SELECT * FROM `Barang`'
+        'SELECT `image` FROM `Barang`'
     );
-	$f3->set('array_barang',$result);
-	$view=new View;
-    echo $view->render('pages/user/home.html');
+	$array = array();
+	foreach ($result as $value) {
+		foreach ($value as $values) {
+			array_push($array,$values);
+		}
+	}
+	$f3->set('gambar',$array);
+	echo \Template::instance()->render('pages/user/home.html');
 });
 
 $f3->route('GET /product',function($f3){
-	$view=new View;
-    echo $view->render('pages/user/product.html');
+	$_SESSION['username'] = "chrisandyarief";
+	var_dump($_SESSION['username']);
+	$result=$f3->get("DB")->exec(
+        'SELECT `name`,`image`,`description`,`price` FROM `barang`'
+    );
+	$array = array();
+	foreach ($result as $value) {
+		array_push($array,$value);
+	}
+	$f3->set('dataproduct',$array);
+	echo \Template::instance()->render('pages/user/product.html');
 });
-$f3->route('GET /cart',function($f3){
-	$view=new View;
-    echo $view->render('pages/user/cart.html');
-});
+// $f3->route('GET /cart',function($f3){
+// 	echo \Template::instance()->render('pages/user/cart.html');
+// });
 $f3->route('GET /about',function($f3){
-	$view=new View;
-    echo $view->render('pages/user/about.html');
+	echo \Template::instance()->render('pages/user/about.html');
 });
 $f3->route('GET /contact',function($f3){
-	$view=new View;
-    echo $view->render('pages/user/contact.html');
+	echo \Template::instance()->render('pages/user/contact.html');
+});
+//ajax routing
+$f3->route('POST /addToCart',function($f3){
+	if (isset($_SESSION['username'])) {
+		$sqlFindId = 'SELECT `id` FROM user WHERE `username` ="'.$_SESSION['username'].'"';
+		$temp = $f3->get("DB")->exec($sqlFindId);
+		$productName  =  $_POST;
+		$tempId;
+		foreach ($temp as $value) {
+			$tempId = $value['id'];
+		}
+		foreach ($productName as $key => $value) {
+			$productName = str_replace(" ","",$key);
+			$productName = str_replace("_"," ",$key);
+			$productName = ltrim($productName);
+			$productName = rtrim($productName);
+		}
+		$sqlGetCart = 'SELECT `cart` FROM user WHERE `username` ="'.$_SESSION['username'].'"';
+		$tempCart = $f3->get("DB")->exec($sqlGetCart);
+		$tmpCart;
+		foreach ($tempCart as $value) {
+			$tmpCart = $value['cart'];
+		}
+		var_dump($tmpCart);
+		$sql = 'SELECT `id` from barang where `name` ="'."$productName".'"';
+		$result=$f3->get("DB")->exec($sql);
+		$tmp;
+		foreach ($result as $value) {
+			$tmp = $value['id'];
+		}
+		if ($tmpCart == "") {
+			$tmpCart = $tmp;
+		}
+		else{
+			$tmpCart = $tmpCart.", ".$tmp;
+		}
+		$sqlAdd = 'UPDATE user SET cart="'.$tmpCart.'" WHERE `id` = "'.$tempId.'"';
+		$f3->get("DB")->exec($sqlAdd);
+	}
+	else{
+		echo "No User Active";
+	}
 });
 
+//admin routing
 $f3->route('GET /adminHome',function($f3){
-	$view=new View;
-    echo $view->render('pages/admin/adminhome.html');
+	echo \Template::instance()->render('pages/admin/adminhome.html');
 });
 
 $f3->route('GET /adminUser',function($f3){
-	$view=new View;
-    echo $view->render('pages/admin/user.html');
+	echo \Template::instance()->render('pages/admin/user.html');
 });
 $f3->route('GET /adminPayment',function($f3){
-	$view=new View;
-    echo $view->render('pages/admin/payment.html');
+	echo \Template::instance()->render('pages/admin/payment.html');
 });
 $f3->route('GET /adminOrder',function($f3){
-	$view=new View;
-    echo $view->render('pages/admin/order.html');
+	echo \Template::instance()->render('pages/admin/order.html');
 });
 
-$f3->route('GET /userref',
-	function($f3) {
-		$f3->set('content','userref.htm');
-		echo View::instance()->render('layout.htm');
-	}
-);
-
-//server
+// server
 // $f3->set("DB",new DB\SQL(
 //     'mysql:host=localhost;port=3306;dbname=id7413042_bukantokosebelah',
 //     'id7413042_admin',
 //     'admin123'
 // ));
 
-//local
+// //local
 $f3->set("DB",new DB\SQL(
     'mysql:host=localhost;port=3306;dbname=e-commerce',
     'root',
